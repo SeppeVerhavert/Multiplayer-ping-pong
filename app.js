0003ee33bd938702a1a9b9186e436711cad4a59b
seppe_verhavert <seppe.verhavert@gmail.com>
2019-08-22 09:19:11 +0200

    add css to public

--- app.js
+++ app.js
@@ -1,24 +1,59 @@
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
-// var io = require('socket.io')(server);
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/Client/index.html');
+  res.sendFile(__dirname + '/public/index.html')
 });
-app.use('/client', express.static(__dirname + '/client'));
+app.use('/public', express.static(__dirname + '/public'));
+app.use(express.static('public'));
 
 server.listen(3000);
 console.log("server started.");
 
 var socket_list = {};
+var player_list = {};
+
+var Player = function (id) {
+  var self = {
+    x: 250,
+    y: 250,
+    id: id,
+    number: "" + Math.floor(10 * Math.random()),
+    pressingRight: false,
+    pressingLeft: false,
+    pressingUp: false,
+    pressingDown: false,
+    maxSpd: 10,
+  }
+  self.updatePosition = function () {
+    if (self.pressingRight)
+      self.x += self.maxSpd;
+
+    if (self.pressingLeft)
+      self.x -= self.maxSpd;
+
+    if (self.pressingUp)
+      self.x -= self.maxSpd;
+
+    if (self.pressingDown)
+      self.x += self.maxSpd;
+  }
+  return self;
+}
 
 var io = require('socket.io')(server);
 io.sockets.on('connection', function (socket) {
   socket.id = Math.random();
-  socket.x = 0;
-  socket.y = 0;
   socket_list[socket.id] = socket;
+
+  var player = Player(socket.id);
+  player_list[socket.id] = player;
+
+  socket.on('disconnect', function () {
+    delete socket_list[socket.id];
+    delete player_list[socket.id];
+  });
 
   // console.log('happy because ' + data.reason);
 
@@ -33,16 +68,16 @@ io.sockets.on('connection', function (socket) {
 
 setInterval(function () {
   var pack = [];
-  for (var i in socket_list) {
-    var socket = socket_list[i];
-    socket.x++;
-    socket.y++;
+  for (var i in player_list) {
+    var player = player_list[i];
+    player.updatePosition();
     pack.push({
-      x: socket.x,
-      y: socket.y
+      x: player.x,
+      y: player.y,
+      number: player.number
     });
-  }  
-  for(var i in socket_list) {
+  }
+  for (var i in socket_list) {
     var socket = socket_list[i];
     socket.emit('newPositions', pack);
   }
--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,63 @@
+let ctx = document.getElementById("ctx").getContext("2d");
+    ctx.font = '30px Arial';
+    
+    var socket = io();
+
+    socket.on('newPositions', function(data){
+        ctx.clearRect(0,0,500,500);
+        for(var i = 0; i < data.length; i++) {
+            ctx.fillText(data[i].number,data[i].x, data[i].y);
+        }
+    });
+
+    document.onkeydown = function(event){
+        if (event.keyCode === 39){  //right
+            socket.emit('keyPress', {
+                inputId:'right', state:true});
+        } else if (event.keyCode === 37){  //left
+            socket.emit('keyPress', {
+                inputId:'left', state:true});
+        } else if (event.keyCode === 38){  //up
+            socket.emit('keyPress', {
+                inputId:'up', state:true});
+        } else if (event.keyCode === 40){  //down
+            socket.emit('keyPress', {
+                inputId:'down', state:true});
+        } 
+    }
+
+    document.onkeyup = function(event){
+        if (event.keyCode === 39){  //right
+            socket.emit('keyPress', {
+                inputId:'right', state:false});
+        } else if (event.keyCode === 37){  //left
+            socket.emit('keyPress', {
+                inputId:'left', state:false});
+        } else if (event.keyCode === 38){  //up
+            socket.emit('keyPress', {
+                inputId:'up', state:false});
+        } else if (event.keyCode === 40){  //down
+            socket.emit('keyPress', {
+                inputId:'down', state:false});
+        } 
+    }
+    // var random = Math.random();
+
+    // var happy = function () {
+    //     socket.emit('happy', {
+    //         reason: "it's my birthday" + random
+    //     });
+    // }
+
+    // socket.on('serverMsg', function (data) {
+    //     console.log(data.msg);
+    // });
+
+    
 //          VARIABLES           //
 
 
-let canvas = document.getElementById("myCanvas");
+let canvas = document.getElementById("ctx");
 let ctx = canvas.getContext("2d");
 
 let x = 500;

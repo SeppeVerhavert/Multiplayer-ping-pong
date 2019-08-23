9d1bf0bac15fe4ac5f445d8485578e19ec65fa17
seppe_verhavert <seppe.verhavert@gmail.com>
2019-08-23 10:27:41 +0200

    add collision detection

--- app.js
+++ app.js
@@ -21,6 +21,18 @@ console.log("server started.");
 var socket_list = {};
 var player_list = {};
 
+var ballpack = {};
+var serverX = 500;
+var serverY = 250;
+var serverDx = 3;
+var serverDy = 3;
+ballpack = { 
+  serverX: serverX, 
+  serverY: serverY, 
+  serverDx: serverDx, 
+  serverDy: serverDy 
+};
+
 var Player = function (id) {
   var self = {}
   return self;
@@ -28,28 +40,29 @@ var Player = function (id) {
 
 io.on('connection', function (socket) {
   console.log("player " + socket.id + " connected");
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
 
   var player = Player(socket.id);
   player_list[socket.id] = player;
-
-  // socket.on('updateServer', function () {
-  //   x = serverX;
-  //   y = serverY;
-  // });
 
   socket.on('disconnect', function () {
     console.log(socket.id + " disconnected");
     delete socket_list[socket.id];
     delete player_list[socket.id];
   });
+
+
+  socket.on('clientUpdate', function (data) {
+    serverX = data.clientX + serverDx;
+    serverY = data.clientY + serverDy;
+    serverDx = data.clientDx;
+    serverDy = data.clientDy;
+    ballpack = { 1: serverX, 2: serverY, 3: serverDx, 4: serverDy};
+  });
+
+  setInterval(() => {
+    socket.emit('serverUpdate', ballpack);
+  }, 1000 / 25);
 });
 
-// setInterval(() => {
-//   io.sockets.emit("updateClient", {
-//     serverX: x, 
-//     serverY: y });
-//   }, 1000 / 25);
+
+

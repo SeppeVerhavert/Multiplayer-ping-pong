56e34f53d45c7911de1c07794120e6e0b95f3d25
seppe_verhavert <seppe.verhavert@gmail.com>
2019-08-27 11:45:48 +0200

    put logic on server side

--- app.js
+++ app.js
@@ -18,11 +18,13 @@ app.use(express.static('public'));
 server.listen(3000);
 console.log("server started.");
 
+
+//  SET LISTS AND VARIABLES
 let socket_list = {};
 let player_list = [];
 let i = 0;
 
-let multiPlayer = false;
+// let multiPlayer = false;
 
 let updatePack = {};
 let serverX = 500;
@@ -30,22 +32,13 @@ let serverY = 250;
 let serverDx = 3;
 let serverDy = 3;
 
-let serverPalletYP1 = 220;
-let serverPalletYP2 = 220;
+// let serverPalletYP1 = 220;
+// let serverPalletYP2 = 220;
 
-let serverScoreP1 = 0;
-let serverScoreP2 = 0;
+// let serverScoreP1 = 0;
+// let serverScoreP2 = 0;
 
-updatePack = {
-  serverX: serverX,
-  serverY: serverY,
-  serverDx: serverDx,
-  serverDy: serverDy,
-  serverPalletYP1: serverPalletYP1,
-  serverPalletYP2: serverPalletYP2,
-  serverScoreP1: serverScoreP1,
-  serverScoreP2: serverScoreP2,
-}
+//  MAKE SOCKET ON CONNECTION
 
 io.on('connection', function (socket) {
   console.log("player " + socket.id + " connected");
@@ -59,37 +52,15 @@ io.on('connection', function (socket) {
     serverDx = data.clientDx;
     serverDy = data.clientDy;
 
-    serverPalletYP1 = data.clientPalletYP1;
-    serverPalletYP2 = data.clientPalletYP2;
+    // serverPalletYP1 = data.clientPalletYP1;
+    // serverPalletYP2 = data.clientPalletYP2;
 
-    serverScoreP1 = data.clientCounterP1;
-    serverScoreP2 = data.clientCounterP2;
+    // serverScoreP1 = data.clientCounterP1;
+    // serverScoreP2 = data.clientCounterP2;
 
-    updatePack = {
-      serverX: serverX,
-      serverY: serverY,
-      serverDx: serverDx,
-      serverDy: serverDy,
-      serverPalletYP1: serverPalletYP1,
-      serverPalletYP2: serverPalletYP2,
-      serverScoreP1: serverScoreP1,
-      serverScoreP2: serverScoreP2,
-    }
-
-    if (multiPlayer === true) {
-      socket.emit('serverUpdate', updatePack);
-    }
+    serverUpdate(data);
+    setTimeout(function(){ socket.emit('serverUpdate', updatePack); }, 1000/60);
   });
-
-  if (Object.keys(player_list).length < 2) {
-    console.log("waiting for opponent");
-    multiPlayer = false;
-    serverScoreP1 = 0;
-    serverScoreP2 = 0;
-  } else if (Object.keys(player_list).length >= 2) {
-    console.log("2 players");
-    multiPlayer = true;
-  }
 
   socket.on('disconnect', function () {
     console.log(socket.id + " disconnected");
@@ -97,7 +68,38 @@ io.on('connection', function (socket) {
     delete player_list[socket.id];
   });
 
-  if (multiPlayer === true) {
-    socket.emit('serverUpdate', updatePack);
-  }
+  updateServerPack();
+  socket.emit('serverUpdate', updatePack);
 });
+
+function updateServerPack() {
+  updatePack = {
+    serverX: serverX,
+    serverY: serverY,
+    serverDx: serverDx,
+    serverDy: serverDy,
+    // serverPalletYP1: serverPalletYP1,
+    // serverPalletYP2: serverPalletYP2,
+    // serverScoreP1: serverScoreP1,
+    // serverScoreP2: serverScoreP2,
+  }
+}
+
+function applyLogic() {
+  serverX += serverDx;
+  serverY += serverDy;
+
+  if (serverY + serverDy > 500 || serverY + serverDy < 0) {
+    serverDy = -serverDy;
+  }
+  if (serverX + serverDx > 1000 || serverX + serverDx < 0) {
+    serverDx = -serverDx;
+  }
+}
+
+function serverUpdate(data) {
+  applyLogic();
+  // console.log("old", updatePack);
+  updateServerPack();
+  // console.log("new", updatePack);
+}
--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,4 @@
-//          VARIABLES           //
+// //         VARIABLES           //
 
 
 let canvas = document.getElementById("ctx");
@@ -10,20 +10,20 @@ let clientDx;
 let clientDy;
 
 let ballDiameter = 10;
-let clientPalletYP1;
-let clientPalletYP2;
-let palletXP1 = 60;
-let palletXP2 = 960;
-let palletwidth = 12;
-let palletHeigth = 100;
+// let clientPalletYP1;
+// let clientPalletYP2;
+// let palletXP1 = 60;
+// let palletXP2 = 960;
+// let palletwidth = 12;
+// let palletHeigth = 100;
 
-let upPressedP1 = false;
-let downPressedP1 = false;
-let upPressedP2 = false;
-let downPressedP2 = false;
+// let upPressedP1 = false;
+// let downPressedP1 = false;
+// let upPressedP2 = false;
+// let downPressedP2 = false;
 
-let clientCounterP1;
-let clientCounterP2;
+// let clientCounterP1;
+// let clientCounterP2;
 
 let savedX = 0;
 let savedY = 0;
@@ -36,20 +36,20 @@ let savedY = 0;
 // let hardMode = false;
 // let difficulty;
 
-//          EVENTLISTENERS           //
+// //         EVENTLISTENERS           //
 
 
-document.addEventListener("keydown", keyDownHandler, false);
-document.addEventListener("keyup", keyUpHandler, false);
+// document.addEventListener("keydown", keyDownHandler, false);
+// document.addEventListener("keyup", keyUpHandler, false);
 
 // easyButton.addEventListener('click', changeDifficulty);
 // mediumButton.addEventListener('click', changeDifficulty);
 // hardButton.addEventListener('click', changeDifficulty);
 
 
-//          FUNCTIONS           //
+//        FUNCTIONS           //
 
-//      DRAW OBJECTS       //
+//        DRAW OBJECTS       //
 
 function drawField() {
     ctx.beginPath();
@@ -73,23 +73,23 @@ function drawBall() {
     ctx.closePath();
 }
 
-function drawPallet1() {
-    ctx.beginPath();
-    ctx.rect(palletXP1, clientPalletYP1, palletwidth, palletHeigth);
-    ctx.fillStyle = "white";
-    ctx.fill();
-    ctx.closePath();
-}
+// function drawPallet1() {
+//     ctx.beginPath();
+//     ctx.rect(palletXP1, clientPalletYP1, palletwidth, palletHeigth);
+//     ctx.fillStyle = "white";
+//     ctx.fill();
+//     ctx.closePath();
+// }
 
-function drawPallet2() {
-    ctx.beginPath();
-    ctx.rect(palletXP2, clientPalletYP2, palletwidth, palletHeigth);
-    ctx.fillStyle = "white";
-    ctx.fill();
-    ctx.closePath();
-}
+// function drawPallet2() {
+//     ctx.beginPath();
+//     ctx.rect(palletXP2, clientPalletYP2, palletwidth, palletHeigth);
+//     ctx.fillStyle = "white";
+//     ctx.fill();
+//     ctx.closePath();
+// }
 
-// //      RANDOM STARTING POINT        //
+//      RANDOM STARTING POINT        //
 
 function randomDirection() {
     let random1 = Math.floor(Math.random() * 2);
@@ -125,41 +125,41 @@ function randomVelocity() {
     }
 }
 
-//      PLAYER MOVEMENT        //
+// //      PLAYER MOVEMENT        //
 
-function keyDownHandler(e) {
-    //      PLAYER 1      //
-    if (e.keyCode == 83) {
-        upPressedP1 = true;
-    }
-    else if (e.keyCode == 88) {
-        downPressedP1 = true;
-    }
-    //      PLAYER 2      //
-    if (e.key == "Up" || e.key == "ArrowUp") {
-        upPressedP2 = true;
-    }
-    else if (e.key == "Down" || e.key == "ArrowDown") {
-        downPressedP2 = true;
-    }
-}
+// function keyDownHandler(e) {
+//     //      PLAYER 1      //
+//     if (e.keyCode == 83) {
+//         upPressedP1 = true;
+//     }
+//     else if (e.keyCode == 88) {
+//         downPressedP1 = true;
+//     }
+//     //      PLAYER 2      //
+//     if (e.key == "Up" || e.key == "ArrowUp") {
+//         upPressedP2 = true;
+//     }
+//     else if (e.key == "Down" || e.key == "ArrowDown") {
+//         downPressedP2 = true;
+//     }
+// }
 
-function keyUpHandler(e) {
-    //      PLAYER 1      //
-    if (e.keyCode == 83) {
-        upPressedP1 = false;
-    }
-    else if (e.keyCode == 88) {
-        downPressedP1 = false;
-    }
-    //      PLAYER 2      //
-    if (e.key == "Up" || e.key == "ArrowUp") {
-        upPressedP2 = false;
-    }
-    else if (e.key == "Down" || e.key == "ArrowDown") {
-        downPressedP2 = false;
-    }
-}
+// function keyUpHandler(e) {
+//     //      PLAYER 1      //
+//     if (e.keyCode == 83) {
+//         upPressedP1 = false;
+//     }
+//     else if (e.keyCode == 88) {
+//         downPressedP1 = false;
+//     }
+//     //      PLAYER 2      //
+//     if (e.key == "Up" || e.key == "ArrowUp") {
+//         upPressedP2 = false;
+//     }
+//     else if (e.key == "Down" || e.key == "ArrowDown") {
+//         downPressedP2 = false;
+//     }
+// }
 
 //      DRAW CANVAS       //
 
@@ -167,122 +167,115 @@ function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawField();
     drawBall();
-    drawPallet1();
-    drawPallet2();
-    collisionDetection();
-    pointMade();
-    movePlayers();
-    updatescore();
-    clientX += clientDx;
-    clientY += clientDy;
-    // console.log(
-    //     "clientX = " + clientX,
-    //     "clientY = " + clientY,
-    //     "clientDx = " + clientDx,
-    //     "clientDy = " + clientDy,
-    //     "clientPalletYP1 = " + clientPalletYP1,
-    //     "clientPalletYP2 = " + clientPalletYP2
-    //     );
+    // drawPallet1();
+    // drawPallet2();
+    // collisionDetection();
+    // pointMade();
+    // movePlayers();
+    // updatescore();
 }
 
 //      COLLISION DETECTION       //
 
-function collisionDetection() {
-    if (clientX + clientDx > palletXP1 - palletwidth && clientX + clientDx < palletXP1) {
-        if (clientY >= clientPalletYP1 - 5 && clientY <= clientPalletYP1 + palletHeigth) {
-            randomVelocity();
-            clientDx = -clientDx;
-        }
-    } else if (clientX + clientDx > palletXP2 && clientX + clientDx < palletXP2 + palletwidth) {
-        if (clientY >= clientPalletYP2 - 5 && clientY <= clientPalletYP2 + palletHeigth) {
-            randomVelocity();
-            clientDx = -clientDx;
-        }
-    }
-    if (clientY + clientDy > canvas.height || clientY + clientDy < 0) {
-        clientDy = -clientDy;
-    }
-}
+// function collisionDetection() {
+    // if (clientX + clientDx > palletXP1 - palletwidth && clientX + clientDx < palletXP1) {
+    //     if (clientY >= clientPalletYP1 - 5 && clientY <= clientPalletYP1 + palletHeigth) {
+    //         randomVelocity();
+    //         clientDx = -clientDx;
+    //     }
+    // } else if (clientX + clientDx > palletXP2 && clientX + clientDx < palletXP2 + palletwidth) {
+    //     if (clientY >= clientPalletYP2 - 5 && clientY <= clientPalletYP2 + palletHeigth) {
+    //         randomVelocity();
+    //         clientDx = -clientDx;
+    //     }
+    // }
+    // if (clientY + clientDy > canvas.height || clientY + clientDy < 0) {
+    //     clientDy = -clientDy;
+    // }
+    // if (clientX + clientDx > canvas.width || clientX + clientDx < 0) {
+    //     clientDx = -clientDx;
+    // }
+// }
 
 //      CHECK FOR POINTS       //
 
-function pointMade() {
-    if (clientX > canvas.width) {
-        clientCounterP1 += 1;
-        gameOver();
-    } else if (clientX < 0) {
-        clientCounterP2 += 1;
-        gameOver();
-    }
-}
+// function pointMade() {
+//     if (clientX > canvas.width) {
+//         clientCounterP1 += 1;
+//         gameOver();
+//     } else if (clientX < 0) {
+//         clientCounterP2 += 1;
+//         gameOver();
+//     }
+// }
 
-//      PLAYER MOVEMENT       //
+// //      PLAYER MOVEMENT       //
 
-function movePlayers() {
-    if (downPressedP1) {
-        clientPalletYP1 += 7;
-        if (clientPalletYP1 + palletHeigth > canvas.height) {
-            clientPalletYP1 = canvas.height - palletHeigth;
-        }
-    }
-    else if (upPressedP1) {
-        clientPalletYP1 -= 7;
-        if (clientPalletYP1 < 0) {
-            clientPalletYP1 = 0;
-        }
-    }
+// function movePlayers() {
+//     if (downPressedP1) {
+//         clientPalletYP1 += 7;
+//         if (clientPalletYP1 + palletHeigth > canvas.height) {
+//             clientPalletYP1 = canvas.height - palletHeigth;
+//         }
+//     }
+//     else if (upPressedP1) {
+//         clientPalletYP1 -= 7;
+//         if (clientPalletYP1 < 0) {
+//             clientPalletYP1 = 0;
+//         }
+//     }
 
-    if (downPressedP2) {
-        clientPalletYP2 += 7;
-        if (clientPalletYP2 + palletHeigth > canvas.height) {
-            clientPalletYP2 = canvas.height - palletHeigth;
-        }
-    }
-    else if (upPressedP2) {
-        clientPalletYP2 -= 7;
-        if (clientPalletYP2 < 0) {
-            clientPalletYP2 = 0;
-        }
-    }
-}
+//     if (downPressedP2) {
+//         clientPalletYP2 += 7;
+//         if (clientPalletYP2 + palletHeigth > canvas.height) {
+//             clientPalletYP2 = canvas.height - palletHeigth;
+//         }
+//     }
+//     else if (upPressedP2) {
+//         clientPalletYP2 -= 7;
+//         if (clientPalletYP2 < 0) {
+//             clientPalletYP2 = 0;
+//         }
+//     }
+// }
 
 //      START       //
 
-function startGame() {
-    drawField();
-    randomDirection();
-}
+// function startGame() {
+//     drawField();
+//     randomDirection();
+// }
 
 //      START AGAIN       //
 
-function gameOver() {
-    savedX = clientDx;
-    savedY = clientDy;
-    clientX = canvas.width / 2 - 5;
-    clientY = 250;
-    clientDx = 0;
-    clientDy = 0;
-    setTimeout(timer, 3000);
-    clearInterval(drawInterval);
-    clearInterval(window.drawInterval);
-    window.drawInterval = setInterval(draw, 10);
-    updatescore();
-    startGame();
+// function gameOver() {
+//     savedX = clientDx;
+//     savedY = clientDy;
+//     clientX = canvas.width / 2 - 5;
+//     clientY = 250;
+//     clientDx = 0;
+//     clientDy = 0;
+//     setTimeout(timer, 3000);
+//     clearInterval(drawInterval);
+//     clearInterval(window.drawInterval);
+//     window.drawInterval = setInterval(draw, 10);
+//     updatescore();
+//     startGame();
+// }
 
-}
-
-function timer() {
-    clientDx = -savedX;
-    clientDy = savedY;
-    randomVelocity();
-}
+// function timer() {
+//     clientDx = -savedX;
+//     clientDy = savedY;
+//     randomVelocity();
+// }
 
 //      UPDATE SCORE       //
 
-function updatescore() {
-    document.getElementById("scoreP1").innerHTML = clientCounterP1;
-    document.getElementById("scoreP2").innerHTML = clientCounterP2;
-}
+// function updatescore() {
+//     document.getElementById("scoreP1").innerHTML = clientCounterP1;
+//     document.getElementById("scoreP2").innerHTML = clientCounterP2;
+// }
+
 // function changeDifficulty() {
 //     if (this.id = "easyButton") {
 //         easyMode = true;
@@ -302,45 +295,57 @@ function updatescore() {
 // }
 
 
-//          CALL FUNCTIONS           //
+// //          CALL FUNCTIONS           //
 
 
-startGame();
+// startGame();
 window.drawInterval = setInterval(draw, 10);
 
 var socket = io.connect('http://localhost:3000');
+
+socket.on('serverUpdate', function (update) {
+    console.log(
+        "client",
+        clientX,
+        clientY,
+        clientDx,
+        clientDy,
+        //         clientPalletYP1,
+        //         clientPalletYP2,
+        // clientCounterP1,
+        // clientCounterP2,
+        "server",
+        update.serverX,
+        update.serverY,
+        update.serverDx,
+        update.serverDy,
+        //         update.serverPalletYP1,
+        //         update.serverPalletYP2,
+        // update.serverScoreP1,
+        // update.serverScoreP2,
+    );
+});
 
 socket.on('serverUpdate', function (update) {
     clientX = update.serverX;
     clientY = update.serverY;
     clientDx = update.serverDx;
     clientDy = update.serverDy;
-    clientPalletYP1 = update.serverPalletYP1;
-    clientPalletYP2 = update.serverPalletYP2;
-    clientCounterP1 = update.serverScoreP1;
-    clientCounterP2 = update.serverScoreP2;
+    //     clientPalletYP1 = update.serverPalletYP1;
+    //     clientPalletYP2 = update.serverPalletYP2;
+    // clientCounterP1 = update.serverScoreP1;
+    // clientCounterP2 = update.serverScoreP2;
 
-    socket.emit('clientUpdate', {
-        clientX,
-        clientY,
-        clientDx,
-        clientDy,
-        clientPalletYP1,
-        clientPalletYP2,
-        clientCounterP1,
-        clientCounterP2,
-    })
+    setTimeout(function(){
+        socket.emit('clientUpdate', {
+            clientX,
+            clientY,
+            clientDx,
+            clientDy,
+            //         clientPalletYP1,
+            //         clientPalletYP2,
+            // clientCounterP1,
+            // clientCounterP2
+        })
+    }, 10);
 });
-
-// socket.on('serverUpdate', function (update) {
-//     setTimeout(console.log(
-//     update.serverX,
-//     update.serverY,
-//     update.serverDx,
-//     update.serverDy,
-//     update.serverPalletYP1,
-//     update.serverPalletYP2,
-//     update.serverScoreP1,
-//     update.serverScoreP2,
-//     ), 1000)
-// });
